4779f75f8ae3bc0243b17a6e70764a8e00871d92
Gurkarn Goindi <gurkarn.goindi@berkeley.edu>
2020-01-15 12:36:38 +0000

    first commit

---
+++ VCMate/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  VCMate
+//
+//  Created by Gurkarn Goindi on 9/Jan/20.
+//  Copyright © 2020 Gurkarn Goindi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Card {
+    var frontImage : UIImage
+    var rearImage : UIImage
+    var companyName : String
+    var person : String
+}
